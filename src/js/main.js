4a442c01a44434e9246d745d31e03532fe90187e
Vitaliy Andriiv <madara.prog@gmail.com>
2023-01-17 20:10:27 +0200

    fix several  bugs

--- src/js/main.js
+++ src/js/main.js
@@ -43,14 +43,14 @@
 
     const $titlePortfolio = $('.portfolio-title-js');
 
-    $portfolio.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $portfolio.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
         let slidesLength = slick.$slides.length - 1,
             isCurrentFirstOrLast = currentSlide === 0 || currentSlide === slidesLength,
             isNextFirstOrLast = nextSlide === 0 || nextSlide === slidesLength;
 
-        if (isCurrentFirstOrLast && isNextFirstOrLast){
+        if (isCurrentFirstOrLast && isNextFirstOrLast) {
             let nextClone = $(event.currentTarget).find('.slick-cloned.slick-active');
-            setTimeout(function(){
+            setTimeout(function() {
                 nextClone.addClass('slick-current');
             }, 100)
         }
@@ -63,20 +63,25 @@
             opacity: 0,
             top: '30px'
         });
+
         $('.portfolio-subtitle-js').css({
             opacity: 0,
             marginRight: '-80px'
         })
     });
 
+
     $portfolio.on('afterChange', function(event, slick, currentSlide){
         $titlePortfolio.css({
             opacity: 1,
             top: 0
         });
-        $('.glass-visible-js').css({
+
+
+        $('.slick-center .glass-visible-js').css({
             opacity: 1
         });
+
         $('.portfolio-subtitle-js').css({
             opacity: 1,
             marginRight: '-50px'
@@ -95,10 +100,6 @@
         }
     }));
 
-    $('.slider-nav .slick-slide').on('click', function (event) {
-        $('.slider-for').slick('slickGoTo', $(this).data('slickIndex'));
-    });
-
     $('.portfolio-modal-shadow-js').click(function () {
         $('.portfolio-modal-js').css({
             opacity: 0,
@@ -115,4 +116,6 @@
             $('.portfolio-modal-page-js').append(`<img src="${$(this).data('link')}" alt=""/>`);
         });
     });
+
+
 })(jQuery);
