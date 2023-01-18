31eccebc0614641cdd4570aa91ca8c3ff401c8ac
Vitaliy Andriiv <madara.prog@gmail.com>
2023-01-18 23:13:43 +0200

    fix about php

--- about.php
+++ about.php
@@ -1,29 +1,29 @@
 <?php include 'sections/header.php' ?>
 <main class="about about-js">
-<!--    <div class="glass-wrap about__window about__window--1 about-window-js d-flex flex-column">-->
-<!--        <div class="container-fluid">-->
-<!--            <h1 class="title-bread">О нас</h1>-->
-<!--        </div>-->
-<!--        <div class="d-flex justify-content-center">-->
-<!--            <div class="glass">-->
-<!--                <div class="about__title title-mega mb-xl-5">-->
-<!--                    <span class="black">Lynx</span><span class="white">foundation</span>-->
-<!--                </div>-->
-<!--                <p>-->
-<!--                    Команда молодых, креативных людей. Создаем IT‑решения и помогаем-->
-<!--                    цифровизировать любую вашу идею. Среди проектов автоматизация-->
-<!--                    бизнес‑процессов, онлайн‑сервисы и сайты, внедрение digital‑инструментов-->
-<!--                    для продаж и маркетинга.-->
-<!--                </p>-->
-<!--                <p>-->
-<!--                    Наши основные принципы — гибкость мышления, осознанность,-->
-<!--                    прозрачность, скорость и качество работы, высокий клиентский сервис-->
-<!--                    и нацеленность на результат. Работаем с людьми и для людей.-->
-<!--                </p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
     <div class="glass-wrap about__window about__window--1 about-window-js d-flex flex-column">
+        <div class="container-fluid">
+            <h1 class="title-bread">О нас</h1>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="glass">
+                <div class="about__title title-mega mb-xl-5">
+                    <span class="black">Lynx</span><span class="white">foundation</span>
+                </div>
+                <p>
+                    Команда молодых, креативных людей. Создаем IT‑решения и помогаем
+                    цифровизировать любую вашу идею. Среди проектов автоматизация
+                    бизнес‑процессов, онлайн‑сервисы и сайты, внедрение digital‑инструментов
+                    для продаж и маркетинга.
+                </p>
+                <p>
+                    Наши основные принципы — гибкость мышления, осознанность,
+                    прозрачность, скорость и качество работы, высокий клиентский сервис
+                    и нацеленность на результат. Работаем с людьми и для людей.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="glass-wrap about__window about__window--2 about-window-js d-flex flex-column">
         <div class="container-fluid">
             <h1 class="title-bread">О нас</h1>
         </div>
@@ -66,7 +66,7 @@
                             Реализация
                         </h3>
                         <div class="d-flex">
-                            <div class="step__number">1</div>
+                            <div class="step__number">3</div>
                             <p class="step__desc">
                                 Вносим ваши коррективы,
                                 оговариваем сроки и
@@ -79,7 +79,7 @@
                             Достижение результата
                         </h3>
                         <div class="d-flex">
-                            <div class="step__number">2</div>
+                            <div class="step__number">4</div>
                             <p class="step__desc">
                                 Показываем вам
                                 промежуточные результаты
