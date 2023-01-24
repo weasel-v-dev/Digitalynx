7897796bad900d82e8150b433428108f3624397e
Vitaliy Andriiv <madara.prog@gmail.com>
2023-01-24 19:02:56 +0200

    fixed contact page

--- contacts.php
+++ contacts.php
@@ -12,56 +12,60 @@
                     </h2>
                     <div class="row">
                         <div class="col-xl-6">
-                            <h3 class="step__title" align="center">
-                                Свяжитесь с нами в удобном для вас сервисе:
-                            </h3>
-                            <div class="contacts__socials d-flex justify-content-around pt-1 pb-3 mb-3">
-                                <a target="_blank" href="https://www.instagram.com/lynxfoundation/">
-                                    <img src="/assets/img/inst-c.svg" alt="Instagram">
-                                </a>
-                                <a target="_blank" href="#">
-                                    <img src="/assets/img/sk-c.svg" alt="Skype">
-                                </a>
-                                <a target="_blank" href="tg://resolve?domain=SerbianLynx">
-                                    <img src="/assets/img/tg-c.svg" alt="Telegram">
-                                </a>
-                                <a target="_blank" href="tg://resolve?domain=SerbianLynx">
-                                    <img src="/assets/img/vk-c.svg" alt="Telegram">
-                                </a>
+                            <div class="pe-xl-5">
+                                <h3 class="step__title" align="center">
+                                    Свяжитесь с нами в удобном для вас сервисе:
+                                </h3>
+                                <div class="contacts__socials d-flex justify-content-around pt-1 pb-3 mb-3 pt-xxl-2 pb-xxl-4 mb-xxl-5">
+                                    <a target="_blank" href="https://www.instagram.com/lynxfoundation/">
+                                        <img src="/assets/img/inst-c.svg" alt="Instagram">
+                                    </a>
+                                    <a target="_blank" href="#">
+                                        <img src="/assets/img/sk-c.svg" alt="Skype">
+                                    </a>
+                                    <a target="_blank" href="tg://resolve?domain=SerbianLynx">
+                                        <img src="/assets/img/tg-c.svg" alt="Telegram">
+                                    </a>
+                                    <a target="_blank" href="tg://resolve?domain=SerbianLynx">
+                                        <img src="/assets/img/vk-c.svg" alt="Telegram">
+                                    </a>
+                                </div>
+                                <h3 class="step__title" align="center">
+                                    Общие вопросы:
+                                </h3>
+                                <p class="step__desc" align="center">
+                                    <a href="mailto:info@digitalynx.org">info@digitalynx.org</a>
+                                </p>
                             </div>
-                            <h3 class="step__title" align="center">
-                                Общие вопросы:
-                            </h3>
-                            <p class="step__desc" align="center">
-                                <a href="mailto:info@digitalynx.org">info@digitalynx.org</a>
-                            </p>
                         </div>
                         <div class="col-xl-6">
-                            <div class="row contacts__divider">
-                                <div class="col-lg-6 col-xl-12">
-                                    <h3 class="step__title mt-3 mt-lg-0" align="center">
-                                        Локация:
-                                    </h3>
-                                    <p class="step__desc mb-3">
-                                        Мы - удаленная outsource команда со штаб-
-                                        квартирой в Белграде.
-                                    </p>
-                                </div>
-                                <div class="col-lg-6 col-xl-12">
-                                    <h3 class="step__title" align="center">
-                                        Адрес:
-                                    </h3>
-                                    <div class="d-flex contacts__address">
-                                        <div>
-                                            <a target="_blank" href="https://goo.gl/maps/64zikwvPK3aRoNzr9"><img src="/assets/img/map.svg" alt=""></a>
-                                        </div>
-                                        <p class="step__desc">
-                                            <a target="_blank" href="https://goo.gl/maps/64zikwvPK3aRoNzr9">
-                                                Ušće Business Center,
-                                                Bulevar Mihajla Pupina 6,
-                                                Белград 11070, Сербия
-                                            </a>
+                            <div class="ps-xl-5 contacts__divider">
+                                <div class="row">
+                                    <div class="col-lg-6 col-xl-12">
+                                        <h3 class="step__title mt-3 mt-lg-0" align="center">
+                                            Локация:
+                                        </h3>
+                                        <p class="step__desc mb-3 mb-xxl-5" align="center">
+                                            Мы - удаленная outsource команда со штаб-
+                                            квартирой в Белграде.
                                         </p>
+                                    </div>
+                                    <div class="col-lg-6 col-xl-12">
+                                        <h3 class="step__title" align="center">
+                                            Адрес:
+                                        </h3>
+                                        <div class="d-flex contacts__address">
+<!--                                            <div>-->
+<!--                                                <a target="_blank" href="https://goo.gl/maps/64zikwvPK3aRoNzr9"><img src="/assets/img/map.svg" alt=""></a>-->
+<!--                                            </div>-->
+                                            <p class="step__desc" align="center">
+                                                <a target="_blank" href="https://goo.gl/maps/64zikwvPK3aRoNzr9">
+                                                    Ušće Business Center,
+                                                    Bulevar Mihajla Pupina 6,
+                                                    Белград 11070, Сербия
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
