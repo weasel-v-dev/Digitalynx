33b7427d6eed1a24d017287ab6b7ff1409e7e730
Vitaliy Andriiv <madara.prog@gmail.com>
2023-01-24 22:56:59 +0200

    fix link for SEO

--- sections/header.php
+++ sections/header.php
@@ -16,6 +16,7 @@
     <meta property="og:title" content="Создание сайтов любой сложности | Веб-студия Digital Lynx">
     <meta property="og:description" content="Студия веб-дизайна и разработки, создает креативные веб-сайты и разрабатывает потрясающие мобильные и веб-приложения. Приведите друга и получите скидку 25% на любую услугу.">
     <link type="Image/x-icon" href="/assets/img/favicon.ico" rel="icon">
+    <link rel="canonical" href="https://digitalynx.org/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script
